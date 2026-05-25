7ce34d0db8c425295adfc805b30b097d6a614d8e
Karen Leal silva <you@example.com>
2026-05-24 21:11:58 -0500

    Se añadió linea para escribir en archivo

--- labgit.cpp
+++ labgit.cpp
@@ -1,17 +1,16 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
-
 using namespace std;
 int main(){
 	ifstream archivoEntrada ("input/archivoEntrada.txt") ;
-	
-	if (archivoEntrada.is_open()) {
+	ofstream archivoSalida("output/archivoSalida.txt");
+	if (archivoEntrada.is_open() && archivoSalida.is_open()) {
 		cout<<"Archivo abierto"<<endl;
-		string linea;
-		
+		string linea;		
 		while (getline(archivoEntrada, linea)) {
-		}
+		archivoSalida << linea;
+		}		
 			cout<<"Archivo copiado correctamente"<<endl;
 	}
 	
